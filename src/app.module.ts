d9bc8dce99a0f39d901a27ea8dc962c462d84c62
vergaraCoder <jhonatanvergara072@gmail.com>
2025-01-14 08:50:42 -0500

    feat: creation of the module opinions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { OpinionsModule } from './opinions/opinions.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,OpinionsModule],
   controllers: [],
   providers: [
     
