e2f3d3361e4ad0f9974551ae7d00873218aff000
vergaraCoder <jhonatanvergara072@gmail.com>
2025-01-14 09:50:06 -0500

    feat: creation of the entitie and category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OpinionsModule } from './opinions/opinions.module';
 import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [UsersModule,OpinionsModule],
+  imports: [UsersModule,OpinionsModule, CategoriesModule],
   controllers: [],
   providers: [
     
